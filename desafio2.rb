b2ab0b8c40448a8d02a6865703498c5d40f65e64
Jacinto Ernesto <mendesjacinto8@gmail.com>
2023-11-29 16:02:46 +0100

    Segundo desafio

---
+++ desafio2.rb
@@ -0,0 +1,7 @@
+print "Digie o seu nome : "
+nome = gets.chomp
+print "Digete o seu sobrenome : "
+sobrenome = gets.chomp
+print "Digete a sua idade : "
+idade = gets.chomp
+puts " Nome: #{nome} \n Sobrenome: #{sobrenome} \n Idade: #{idade}"
